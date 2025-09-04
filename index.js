9bcfea19988c3e8c1db3a03ba4174385abae0d7a
Juancar1682 <96840752+Juancar1682@users.noreply.github.com>
2025-09-04 18:03:31 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,127 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let password1El = document.getElementById("password1-el");
+let password2El = document.getElementById("password2-el");
+
+function getPassword() {
+  password1El.textContent = "";
+  password2El.textContent = "";
+  for (let i = 0; i < 13; i++) {
+    let randomPassword1 = Math.floor(Math.random() * characters.length);
+    let randomPassword2 = Math.floor(Math.random() * characters.length);
+    password1El.textContent += characters[randomPassword1];
+    password2El.textContent += characters[randomPassword2];
+  }
+}
+
+password1El.addEventListener("click", async () => {
+  const contentToCopy = password1El.textContent;
+  try {
+    await navigator.clipboard.writeText(contentToCopy);
+    console.log("Content copied to clipboard!");
+  } catch (err) {
+    console.error("Failed to copy content: ", err);
+  }
+});
+
+password2El.addEventListener("click", async () => {
+  const contentToCopy = password2El.textContent;
+  try {
+    await navigator.clipboard.writeText(contentToCopy);
+    console.log("Content copied to clipboard!");
+  } catch (err) {
+    console.error("Failed to copy content: ", err);
+  }
+});
